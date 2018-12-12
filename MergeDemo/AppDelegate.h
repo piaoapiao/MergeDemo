374bf57c814d0a6e9bf99bb515a3bd3217d96fb0
guodong <>
2018-12-12 15:25:58 +0800

    for test merge

---
+++ MergeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MergeDemo
+//
+//  Created by guodong on 2018/12/12.
+//  Copyright © 2018年 Maizi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MergeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MergeDemo
+//
+//  Created by guodong on 2018/12/12.
+//  Copyright © 2018年 Maizi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
