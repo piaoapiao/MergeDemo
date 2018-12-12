2968cc059f5169b61984f5eb312265924a04bd20
guodong <guodong@guodongdeMacBook-Pro.local>
2018-12-12 15:31:40 +0800

    add file b

---
+++ MergeDemo/AddFileB.h
@@ -0,0 +1,17 @@
+//
+//  AddFileB.h
+//  MergeDemo
+//
+//  Created by guodong on 2018/12/12.
+//  Copyright © 2018年 Maizi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AddFileB : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MergeDemo/AddFileB.m
@@ -0,0 +1,13 @@
+//
+//  AddFileB.m
+//  MergeDemo
+//
+//  Created by guodong on 2018/12/12.
+//  Copyright © 2018年 Maizi. All rights reserved.
+//
+
+#import "AddFileB.h"
+
+@implementation AddFileB
+
+@end
