6c872c91239695733407b40b5278a030a3f6c41b
guodong <guodong@guodongdeMacBook-Pro.local>
2018-12-25 13:42:52 +0800

    branchEcommit1

--- MergeDemo/AFile.h
+++ MergeDemo/AFile.h
@@ -16,6 +16,8 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nonatomic,retain) NSString *age;
 
 @property (nonatomic,retain) NSString *country;
+
+@property (nonatomic,retain) NSString *branchECommit1;
 @end
 
 NS_ASSUME_NONNULL_END
